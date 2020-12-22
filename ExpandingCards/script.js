30918b396af1094f826875143489b675764fb690
Atavachron <37329317+Atavachron@users.noreply.github.com>
2020-12-23 01:39:00 +0400

    Refactor forEach method

--- ExpandingCards/script.js
+++ ExpandingCards/script.js
@@ -1,14 +1,15 @@
+//Get a node list of all the panels
 const panels = document.querySelectorAll('.panel');
 
+//Use forEach method to iterate through the node list
 panels.forEach(panel => {
+  //Add a click event listenet to each panel
   panel.addEventListener('click', () => {
-    removeActiveClasses();
+    //Remove the "active" class from every panel
+    panels.forEach(panel => {
+      panel.classList.remove('active');
+    });
+    //Add the active class to the clicke element
     panel.classList.add('active');
   });
 });
-
-function removeActiveClasses() {
-  panels.forEach(panel => {
-    panel.classList.remove('active');
-  });
-}
