79c63b25e06000f3e28119e6a2021f2c364738ac
Atavachron <37329317+Atavachron@users.noreply.github.com>
2021-02-28 23:51:10 +0400

    First commit in Mobile Tab Navigation project

--- AutoTextEffect/script.js
+++ AutoTextEffect/script.js
@@ -5,13 +5,17 @@ const text = 'This is auto text effect';
 
 //Variable showing where we are in the text
 let idx = 1;
+//Variable for the speed
 let speed = 300 / speedEl.value;
 
+//Call the function that will write the text
 writeText();
 
 function writeText() {
+  //Set the inner text of the textEl to the first character of the text variable
   textEl.innerText = text.slice(0, idx);
 
+  //Increment the index variable
   idx++;
 
   if (idx > text.length) {
@@ -21,4 +25,5 @@ function writeText() {
   setTimeout(writeText, speed);
 }
 
+//Add event listener to the speed element and change the speed variable based on the selected value
 speedEl.addEventListener('input', e => (speed = 300 / e.target.value));
