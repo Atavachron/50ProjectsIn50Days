992cb5888ce870a27c59abeccacade1ba378135d
Atavachron <37329317+Atavachron@users.noreply.github.com>
2021-03-23 23:59:42 +0400

    Add JS for random messages

---
+++ ToastNotifications/script.js
@@ -0,0 +1,20 @@
+const button = document.getElementById('button');
+const toasts = document.getElementById('toasts');
+
+const messages = [
+  'Message One',
+  'Message Two',
+  'Message Three',
+  'Message Four',
+];
+
+button.addEventListener('click', () => createNotification());
+
+function createNotification() {
+  const notification = document.createElement('div');
+  notification.classList.add('toast');
+
+  notification.innerText = getRandomMessage();
+}
+
+function getRandomMessage() {}
