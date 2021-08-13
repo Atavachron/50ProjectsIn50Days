a892e0b846b9ecbe57b21445f95e85053a76f15f
Atavachron <37329317+Atavachron@users.noreply.github.com>
2021-08-14 01:14:32 +0400

    Add user filter function

--- LiveUserFilter/script.js
+++ LiveUserFilter/script.js
@@ -4,15 +4,18 @@ const listItems = [];
 
 getData();
 
+filter.addEventListener('input', e => {
+  filterData(e.target.value);
+});
+
 async function getData() {
   const res = await fetch('https://randomuser.me/api?results=50');
   const { results } = await res.json();
 
-  results.innerHTML = '';
+  result.innerHTML = '';
 
   results.forEach(user => {
     const li = document.createElement('li');
-
     listItems.push(li);
 
     li.innerHTML = `
@@ -24,5 +27,15 @@ async function getData() {
     `;
 
     result.appendChild(li);
+  });
+}
+
+function filterData(searchTerm) {
+  listItems.forEach(item => {
+    if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+      item.classList.remove('hide');
+    } else {
+      item.classList.add('hide');
+    }
   });
 }
